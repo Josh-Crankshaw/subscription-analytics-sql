a268a2350e67f65d9b65aa48ab9ecd7f13ebef2e
Josh-Crankshaw <joshglencrankshaw@gmail.com>
2026-03-23 17:07:13 +1300

    Completed retention_and_cohort_analysis queries

--- sql/analysis/retention_and_cohort_analysis/02-retention-by-acquisition-channel.sql
+++ sql/analysis/retention_and_cohort_analysis/02-retention-by-acquisition-channel.sql
@@ -1,14 +1,17 @@
--- Calculates the retention of each subscription cohort every month until the end of the year
--- Outputs: The cohort month, the months since cohort was formed, the size of the cohort, the number
+-- Calculates the retention of each subscription cohort by acquisition channel every month until the end of the year
+-- Outputs: The cohort month, the subscription acquisition channel, the months since cohort was formed, the size of the cohort, the number
 -- of subscriptions retained through the month, and the rate of retention through the month.
 WITH
 cohort AS (
 	SELECT
 		subscription_id,
+		acquisition_channel,
 		DATE_TRUNC('month', started_date) AS cohort_month,
 		started_date,
 		ended_date
 	FROM subscriptions
+	JOIN customers
+	ON subscriptions.customer_id = customers.customer_id
 ),
 months AS (
 	SELECT generate_series(
@@ -20,13 +23,15 @@ months AS (
 cohort_sizes AS (
 	SELECT
 		cohort_month,
+		acquisition_channel,
 		COUNT(*) AS cohort_size
 	FROM cohort
-	GROUP BY cohort_month
+	GROUP BY cohort_month, acquisition_channel
 ),
 activity AS (
 	SELECT
 		cohort_month,
+		acquisition_channel,
 		month_start,
 		((DATE_PART('year', month_start) - DATE_PART('year', cohort_month)) * 12
 		+ (DATE_PART('month', month_start) - DATE_PART('month', cohort_month))
@@ -37,15 +42,18 @@ activity AS (
 	ON month_start >= cohort_month
 		AND started_date < (month_start + INTERVAL '1 month')
      	AND (ended_date IS NULL OR ended_date >= month_start)
-    GROUP BY cohort_month, month_start
+    GROUP BY cohort_month, acquisition_channel, month_start
 )
 SELECT 
-	cohort_month,
+	activity.cohort_month,
+	activity.acquisition_channel,
 	months_since,
 	cohort_size,
 	retained_subs,
 	ROUND(retained_subs::numeric / cohort_size, 4) AS retention_rate
 FROM activity 
-JOIN cohort_sizes USING (cohort_month)
+JOIN cohort_sizes 
+ON activity.cohort_month = cohort_sizes.cohort_month
+AND activity.acquisition_channel = cohort_sizes.acquisition_channel
 WHERE months_since BETWEEN 0 AND 11
-ORDER BY cohort_month, months_since;
+ORDER BY cohort_month, acquisition_channel, months_since;
--- sql/analysis/retention_and_cohort_analysis/03-retention-by-plan.sql
+++ sql/analysis/retention_and_cohort_analysis/03-retention-by-plan.sql
@@ -1,10 +1,11 @@
--- Calculates the retention of each subscription cohort every month until the end of the year
--- Outputs: The cohort month, the months since cohort was formed, the size of the cohort, the number
+-- Calculates the retention of each subscription cohort by plan type every month until the end of the year
+-- Outputs: The cohort month, the subscription plan type, the months since cohort was formed, the size of the cohort, the number
 -- of subscriptions retained through the month, and the rate of retention through the month.
 WITH
 cohort AS (
 	SELECT
 		subscription_id,
+		plan_type,
 		DATE_TRUNC('month', started_date) AS cohort_month,
 		started_date,
 		ended_date
@@ -20,13 +21,15 @@ months AS (
 cohort_sizes AS (
 	SELECT
 		cohort_month,
+		plan_type,
 		COUNT(*) AS cohort_size
 	FROM cohort
-	GROUP BY cohort_month
+	GROUP BY cohort_month, plan_type
 ),
 activity AS (
 	SELECT
 		cohort_month,
+		plan_type,
 		month_start,
 		((DATE_PART('year', month_start) - DATE_PART('year', cohort_month)) * 12
 		+ (DATE_PART('month', month_start) - DATE_PART('month', cohort_month))
@@ -37,15 +40,18 @@ activity AS (
 	ON month_start >= cohort_month
 		AND started_date < (month_start + INTERVAL '1 month')
      	AND (ended_date IS NULL OR ended_date >= month_start)
-    GROUP BY cohort_month, month_start
+    GROUP BY cohort_month, plan_type, month_start
 )
 SELECT 
-	cohort_month,
+	activity.cohort_month,
+	activity.plan_type,
 	months_since,
 	cohort_size,
 	retained_subs,
 	ROUND(retained_subs::numeric / cohort_size, 4) AS retention_rate
 FROM activity 
-JOIN cohort_sizes USING (cohort_month)
+JOIN cohort_sizes 
+ON activity.cohort_month = cohort_sizes.cohort_month
+AND activity.plan_type = cohort_sizes.plan_type
 WHERE months_since BETWEEN 0 AND 11
-ORDER BY cohort_month, months_since;
+ORDER BY cohort_month, plan_type, months_since;
